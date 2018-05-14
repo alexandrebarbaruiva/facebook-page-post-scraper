09a64097b6701e85a0e747be9b01c9adcc799f15
luan mgf <luan.mgf@gmail.com>
2018-05-14 18:09:43 -0300

    heroku: change schedule

--- heroku_clock.py
+++ heroku_clock.py
@@ -2,7 +2,7 @@ from apschedule.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_weed='mon-fri', hour=20, min=46)
+@sched.scheduled_job('cron', day_of_weed='mon-fri', hour=21, min=11)
 def schedule_job():
 	from .scraper import collector
 	collector.collect_all_pages()
