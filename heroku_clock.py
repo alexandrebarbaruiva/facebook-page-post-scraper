f86269481b1cb99798c33e79519149b2d1c76993
luan mgf <luan.mgf@gmail.com>
2018-05-15 15:47:49 -0300

    agendamento

--- heroku_clock.py
+++ heroku_clock.py
@@ -2,7 +2,7 @@ from apschedule.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_weed='mon-fri', hour=15, min=10)
+@sched.scheduled_job('cron', day_of_weed='mon-fri', hour=15, min=15)
 def schedule_job():
 	from .scraper import collector
 	collector.collect_all_pages()
