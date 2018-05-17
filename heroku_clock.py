cd435a9efeb30681e0fef2a727b1aff30b7536a5
Luan Freitas <luan.mgf@gmail.com>
2018-05-17 19:36:58 -0300

    Adição do procedimento de coleta de token

--- heroku_clock.py
+++ heroku_clock.py
@@ -1,12 +1,14 @@
 from apscheduler.schedulers.background import BlockingScheduler
 from scraper.collector import collect_all_pages
+from scraper.token_manager import collect_token
 
 sched = BlockingScheduler()
 
 def job_function():
 	print("test")
+	collect_token()
 	collect_all_pages()
 
-sched.add_job(job_function,'cron', day_of_week='mon-fri', hour=9, minute=38)
+sched.add_job(job_function,'cron', day_of_week='mon-fri', hour=19, minute=28)
 
 sched.start()
