6f6c5a45c52d1dbe8690bf845c35dafb4db55dbc
Luan Freitas <luan.mgf@gmail.com>
2018-05-20 11:35:46 -0300

    Finalização da funcionalidade Heroku Clock

--- heroku_clock.py
+++ heroku_clock.py
@@ -8,33 +8,9 @@ home = Path.home()
 sched = BlockingScheduler()
 
 def job_function():
-	print("test")
-	csv_dir = home.joinpath('csv')
-	json_dir = home.joinpath('json')
-	if not csv_dir.is_dir():
-		csv_dir.mkdir()
-	if not json_dir.is_dir():
-		json_dir.mkdir()
+	collect_token()
+	collect_all_pages()
 
-	#collect_token()
-	#collect_all_pages()
-
-sched.add_job(job_function,'cron', day_of_week='mon-sun', hour=21, minute=32)
-
-
-#@sched.scheduled_job('interval', minutes=3)
-#def timed_job():
-    #print('This job is run every three minutes.')
-
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=21, minute=33)
-def scheduled_job():
-    print('This job is run every weekday at 5pm.')
-    csv_dir = home.joinpath('csv')
-    json_dir = home.joinpath('json')
-    if not csv_dir.is_dir():
-    	csv_dir.mkd()
-    if not json_dir.is_dir():
-    	json_dir.mkdir()
-
+sched.add_job(job_function,'cron', day_of_week='mon-sun', hour=11, minute=1)
 
 sched.start()
