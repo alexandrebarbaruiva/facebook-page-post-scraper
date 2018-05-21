35249d40bc2915d5192107f26970e867c6cbab54
gabrielevi10 <gabrielevigomes@gmail.com>
2018-05-21 15:09:49 -0300

    Testando se home aparece no heroku web corretamente

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python -m server.server 
+web: python -m server.main 
--- server/main.py
+++ server/main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def hello():
     return "Facebook Scraper."
 
-@app.route('/actor/<actor_name>', methods=['GET'])
+@app.route('/actors', methods=['GET'])
 def actor_name(actor_name):
     with open('json/actors.json') as actors:
         return jsonify(json.load(actors)) 
@@ -18,6 +18,7 @@ def actor_name(actor_name):
 # def actor_name_json(actor_name):
 #     return jsonify({'actor_name' : actor_name})
 
+port = int(os.environ.get('PORT', 5000))
+
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', debug=True)
