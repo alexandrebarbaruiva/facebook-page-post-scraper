7b38b585a0bad4a6aab8246193bad9ec560ae30c
gabrielevi10 <gabrielevigomes@gmail.com>
2018-06-06 23:33:13 -0300

    resolvendo style

--- server/rest.py
+++ server/rest.py
@@ -3,13 +3,16 @@ import json
 import os
 import sys
 sys.path.append(
-    os.path.dirname(os.path.realpath(__file__))
+    os.path.dirname(
+        os.path.dirname(os.path.realpath(__file__))
+    )
 )
 from server.service import DBService
 
 
 app = Flask(__name__)
 access_db = DBService()
+
 
 @app.route("/")
 def hello():
