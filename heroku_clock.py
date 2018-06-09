2faaea0db835d37aa4ab1058fbdc86cf149e6e60
Luan Freitas <luan.mgf@gmail.com>
2018-06-09 18:46:15 -0300

    Limpeza de Wainings

--- heroku_clock.py
+++ heroku_clock.py
@@ -1,7 +1,5 @@
 from apscheduler.schedulers.background import BlockingScheduler
-from scraper.collector import collect_all_pages
-from scraper.token_manager import collect_token
-#from scraper.new_data_collector import collect_new_data
+from scraper.collector import collect_all_pages, collect_new_data
 
 from pathlib import Path
 home = Path.home()
@@ -10,7 +8,7 @@ sched = BlockingScheduler()
 
 def job_function():
 	collect_all_pages()
-	#collect_new_data()
+	collect_new_data();
 
 sched.add_job(job_function,'cron', day_of_week='mon-sun', hour=11, minute=1)
 
