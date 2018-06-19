ef898d160f1e6e9116095ffb953ce33f83cf9fe8
gabrielevi10 <gabrielevigomes@gmail.com>
2018-06-19 10:00:15 -0300

    A

--- heroku_clock.py
+++ heroku_clock.py
@@ -13,6 +13,6 @@ def job_function():
 	# collect_new_data()
 	print("Fim do coleta de dados")
 
-sched.add_job(job_function,'cron', day_of_week='mon-sun', hour=13, minute=2)
+sched.add_job(job_function,'cron', day_of_week='mon-sun', hour=13, minute=4)
 
 sched.start()
